71a478e1b9d7d8b1fa65c56744b67fab86e9b067
MohnishPurella <purella1597@gmail.com>
2024-05-21 16:56:45 +0530

    Image slider has been implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoaderComponent } from './Components/loader/loader.component';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './Components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoaderComponent, CommonModule],
+  imports: [RouterOutlet, LoaderComponent, CommonModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
