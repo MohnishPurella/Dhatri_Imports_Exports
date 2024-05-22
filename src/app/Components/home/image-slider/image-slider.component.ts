70b93c422bccf782fff59339aaed2fafd96f3815
MohnishPurella <purella1597@gmail.com>
2024-05-22 10:21:15 +0530

    Commented complete code

--- src/app/Components/home/image-slider/image-slider.component.ts
+++ src/app/Components/home/image-slider/image-slider.component.ts
@@ -9,46 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './image-slider.component.css'
 })
 export class ImageSliderComponent {
-  images = [
-    'assets/Images/Home Page Slider/front-view-chestnuts-cup-with-hazelnuts-walnuts.jpg', 
-    'assets/Images/Home Page Slider/IMG_6238.jpg', 
-    'assets/Images/Home Page Slider/istockphoto-867747178-2048x2048.jpg', 
-    'assets/Images/Home Page Slider/macadamia-soap-skin-care-treatment.jpg'
-  ];
-  currentIndex = 0;
-  intervalId: any;
-
-  constructor() { }
-
-  ngOnInit(): void {
-    this.startAutoSliding();
-  }
-
-  slideNext() {
-    const nextIndex = (this.currentIndex + 1) % this.images.length;
-    this.setCurrentIndex(nextIndex);
-  }
-
-  slidePrev() {
-    const prevIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
-    this.setCurrentIndex(prevIndex);
-  }
-
-  setCurrentIndex(index: number) {
-    this.currentIndex = index;
-  }
-
-  switchImage(index: number) {
-    this.setCurrentIndex(index);
-  }
-
-  startAutoSliding() {
-    this.intervalId = setInterval(() => {
-      this.slideNext();
-    }, 3000);
-  }
-
-  stopAutoSliding() {
-    clearInterval(this.intervalId);
-  }
+  
 }
--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -10,51 +9,9 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  isTransparent: boolean = true;  // Declare the isTransparent variable
 
-  constructor(
-    private router: Router, 
-    private renderer: Renderer2, 
-    private el: ElementRef) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    // Subscribe to router events to handle URL changes
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.updateNavbarStyle(event.url);
-      }
-    });
-
-    // Set initial style based on current URL
-    this.updateNavbarStyle(this.router.url);
-  }
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const threshold = 80; // Pixels scrolled before changing style
-    const windowScroll = window.pageYOffset;
-    if (windowScroll > threshold) {
-      this.isTransparent = false;
-      this.applyStyles();
-    } else {
-      this.updateNavbarStyle(this.router.url);
-    }
-  }
-
-  private updateNavbarStyle(url: string) {
-    // Check if the current URL is the home page
-    this.isTransparent = url === '/home' || url === '/';
-    this.applyStyles();
-  }
-
-  private applyStyles() {
-    // Apply styles using Renderer2 based on isTransparent
-    if (this.isTransparent) {
-      this.renderer.addClass(this.el.nativeElement, 'bg-transparent');
-      this.renderer.removeClass(this.el.nativeElement, 'bg-dark');
-    } else {
-      this.renderer.removeClass(this.el.nativeElement, 'bg-transparent');
-      this.renderer.addClass(this.el.nativeElement, 'bg-dark');
-    }
-  }
+  ngOnInit(): void {}
+  
 }
