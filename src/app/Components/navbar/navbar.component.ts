51c98f34f79f50e0443905908b44ec40ad5b624a
MohnishPurella <purella1597@gmail.com>
2024-05-22 06:35:02 +0530

    Navbar has been implemented

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isTransparent: boolean = true;  // Declare the isTransparent variable
 
+  constructor(
+    private router: Router, 
+    private renderer: Renderer2, 
+    private el: ElementRef) {}
+
+  ngOnInit(): void {
+    // Subscribe to router events to handle URL changes
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.updateNavbarStyle(event.url);
+      }
+    });
+
+    // Set initial style based on current URL
+    this.updateNavbarStyle(this.router.url);
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const threshold = 80; // Pixels scrolled before changing style
+    const windowScroll = window.pageYOffset;
+    if (windowScroll > threshold) {
+      this.isTransparent = false;
+      this.applyStyles();
+    } else {
+      this.updateNavbarStyle(this.router.url);
+    }
+  }
+
+  private updateNavbarStyle(url: string) {
+    // Check if the current URL is the home page
+    this.isTransparent = url === '/home' || url === '/';
+    this.applyStyles();
+  }
+
+  private applyStyles() {
+    // Apply styles using Renderer2 based on isTransparent
+    if (this.isTransparent) {
+      this.renderer.addClass(this.el.nativeElement, 'bg-transparent');
+      this.renderer.removeClass(this.el.nativeElement, 'bg-dark');
+    } else {
+      this.renderer.removeClass(this.el.nativeElement, 'bg-transparent');
+      this.renderer.addClass(this.el.nativeElement, 'bg-dark');
+    }
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LoaderComponent } from './Components/loader/loader.component';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './Components/home/home.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoaderComponent, CommonModule, HomeComponent],
+  imports: [RouterOutlet, LoaderComponent, CommonModule, HomeComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
