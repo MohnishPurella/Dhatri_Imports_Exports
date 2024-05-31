81b8f32b19880e01e8c247cc5c827c9885c691ed
MohnishPurella <purella1597@gmail.com>
2024-05-31 15:31:07 +0530

    Content 2 has been implemented

--- src/app/Components/home/content2/content2.component.ts
+++ src/app/Components/home/content2/content2.component.ts
@@ -1,12 +1,14 @@
-import { Component, HostBinding } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostBinding, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-content2',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './content2.component.html',
   styleUrl: './content2.component.css'
 })
 export class Content2Component {
+  showAnimation = false;
   @HostBinding('style.backgroundColor') backgroundColor = '#14ff72cb !important';
 }
